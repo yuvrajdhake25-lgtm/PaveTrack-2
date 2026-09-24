eafc55347da92ea53586f586073c94e620f7b9cf
yuvrajdhake25-lgtm <yuvrajdhake25@gmail.com>
2026-09-24 08:13:11 +0530

    Fix deployment: bcrypt, uvicorn[standard], pymongo[srv], CORS ports

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:8000",
+        "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
         "http://localhost:5175",
